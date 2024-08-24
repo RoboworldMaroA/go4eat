6bfccb1a82067a009ff50e6317d78b05bbfcefd9
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2024-08-24 16:14:06 +0100

    update

---
+++ javascript/about_us.js
@@ -0,0 +1,14 @@
+fetch("https://smartluggagebackend.herokuapp.com/api/v1/trip").then((response) => response.json()).then((TripData) => {
+    // fetch("api/v1/trip/").then((response) => response.json()).then((TripData) => {
+    // console.log("List of items in the trip", TripData);
+    //setTripData(TripData);
+    // calculateLastTripId();
+});
+
+
+fetch("https://smartluggagebackend.herokuapp.com/displayUsers").then((response) => response.json()).then((TripData) => {
+    // fetch("api/v1/trip/").then((response) => response.json()).then((TripData) => {
+    // console.log("List of items in the trip", TripData);
+    //setTripData(TripData);
+    // calculateLastTripId();
+});
