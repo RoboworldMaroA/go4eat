d0752813f442466fd6daa296697beba380a10809
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2024-10-03 20:08:52 +0100

    add appear Into Our World when user scroll down to the tex

--- javascript/about_us.js
+++ javascript/about_us.js
@@ -14,6 +14,24 @@
 // });
 
 
+// function typeWriter(text, elementId, delay) {
+//     let i = 0;
+//     const element = document.getElementById(elementId);
+//     function type() {
+//         if (i < text.length) {
+//             element.innerHTML += text.charAt(i);
+//             i++;
+//             setTimeout(type, delay);
+//         }
+//     }
+//     type();
+// }
+
+// typeWriter("Into Our World", "typewriter", 100);
+
+
+
+
 function typeWriter(text, elementId, delay) {
     let i = 0;
     const element = document.getElementById(elementId);
@@ -27,4 +45,18 @@ function typeWriter(text, elementId, delay) {
     type();
 }
 
-typeWriter("Into Our World", "typewriter", 100);
+document.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                const element = entry.target;
+                element.classList.remove('hidden');
+                typeWriter("Into Our World", "typewriter", 100);
+                observer.unobserve(element);
+            }
+        });
+    });
+
+    const target = document.getElementById('typewriter');
+    observer.observe(target);
+});
