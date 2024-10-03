578e15fd79daeda1cf12beb9a2232510ed34c01a
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2024-10-03 19:49:25 +0100

    add orange to navabar

--- javascript/about_us.js
+++ javascript/about_us.js
@@ -12,3 +12,19 @@
 //     //setTripData(TripData);
 //     // calculateLastTripId();
 // });
+
+
+function typeWriter(text, elementId, delay) {
+    let i = 0;
+    const element = document.getElementById(elementId);
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, delay);
+        }
+    }
+    type();
+}
+
+typeWriter("Into Our World", "typewriter", 100);
