8eabb0f4604fbd5fd7a0a0e355e1d19945e15c47
RoboworldMaroA <augustynmarek@yahoo.co.uk>
2024-10-11 18:52:20 +0100

    change index

--- javascript/index.js
+++ javascript/index.js
@@ -12,3 +12,32 @@
 //     //setTripData(TripData);
 //     // calculateLastTripId();
 // });
+
+
+
+
+
+const text = document.getElementById('paragraphGo4Eat').innerText;
+const paragraph = document.getElementById('paragraphGo4Eat');
+paragraph.innerHTML = '';
+
+for(let i = 0; i < text.length; i++) {
+    const span = document.createElement('span');
+    span.innerText = text[i];
+    paragraph.appendChild(span);
+}
+
+function changeColor(index) {
+    if(index < paragraph.children.length) {
+        paragraph.children[index].classList.add('highlight');
+        setTimeout(() => changeColor(index + 1), 1000);
+    }
+}
+
+
+
+
+
+
+
+changeColor(0);
